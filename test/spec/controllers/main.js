b2c1e290794a3c2dd0bfa43a39554d2fec42dd01
MrDHat <ktyaks@gmail.com>
2013-06-22 04:21:25 +0530

    Added tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,20 +3,24 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('FeedzoApp'));
+  beforeEach(module('FeedzoApp', 'mockedFeed'));
 
   var MainCtrl,
-    scope;
+    scope, mockedFeed, httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend, defaultJSON) {
+    httpBackend = $httpBackend;
+    $httpBackend.whenJSONP(/query.yahooapis.com/).respond(defaultJSON);
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should should have a list of feeds', function () {
+    expect(scope.feeds.length).toBe(1);
+    httpBackend.flush();
+    expect(scope.feeds[0].items[0].title).toBe('Node Roundup: 0.11.2, 0.10.6, subscribe, Omelette');
   });
 });
