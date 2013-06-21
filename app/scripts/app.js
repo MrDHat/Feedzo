adeb76698b7b0973bc241e6a93f15743408e4566
MrDHat <ktyaks@gmail.com>
2013-06-21 22:32:02 +0530

    Feeds rendered

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('FeedzoApp', [])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
